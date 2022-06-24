9eef4f42a2101b72ef0c342323f0b4dfcfaaa852
Kayhan <dev.kayhanozturk@gmail.com>
2022-06-24 08:08:38 +0300

    first inial commit

---
+++ PhotoappApiUsers /src/main/java/com/photoapp/api/users/PhotoappApiUsers/PhotoappApiUsersApplication.java
@@ -0,0 +1,15 @@
+package com.photoapp.api.users.PhotoappApiUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoappApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappApiUsersApplication.class, args);
+	}
+
+}
---
+++ PhotoappApiUsers /src/test/java/com/photoapp/api/users/PhotoappApiUsers/PhotoappApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.api.users.PhotoappApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
